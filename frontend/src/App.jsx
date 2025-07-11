6481d5c34e232238e165ed5d11fd544750bc3edc
PALAKSINGH2828 <ps6953097@gmail.com>
2025-07-11 20:14:40 +0530

    error resolve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,7 +52,16 @@ const App = () => {
           <Navigate to={!isAuthenticated ? "/login" : "/onboarding"}/> 
         )}/>
         <Route path = "/call" element={isAuthenticated ? <CallPage/> : <Navigate to = "/login"/>}/>
-        <Route path = "/chat" element = {isAuthenticated ? <ChatPage/> : <Navigate to = "/login"/>}/>
+        <Route path = "/chat" element = {
+            isAuthenticated && isOnboarded ? (
+              <Layout showSidebar={false}>
+                <ChatPage />
+              </Layout>
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
+        />
         <Route
           path="/onboarding"
           element={
