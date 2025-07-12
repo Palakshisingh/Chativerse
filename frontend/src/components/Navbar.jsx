42b6dfec8a458814534b3c7ea925e7d2a24c11ab
shrash9 <shrasti.20225084@mnnit.ac.in>
2025-07-12 14:06:47 +0530

    Added no of notifications as alert in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,11 +3,29 @@ import useAuthUser from "../hooks/useAuthUser"
 import { BellIcon, Codesandbox, LogOutIcon } from "lucide-react"
 import ThemeSelector from "./ThemeSelector"
 import useLogout from "../hooks/useLogout"
+import { getFriendRequest,sendFriendRequest } from "../lib/api"
+import {React,useEffect,useState} from "react";
+import { useQuery } from "@tanstack/react-query"
+
 
 const Navbar = () => {
   const {authUser} = useAuthUser()
   const location = useLocation()
   const isChatPage=location.pathname?.startsWith("/chat");
+
+  const [friendRequestCount, setFriendRequestCount] = useState(0);
+  useEffect(()=>{
+    async function fetchFriendRequests(){
+      try {
+        const data = await getFriendRequest();
+        setFriendRequestCount(data.incomingRequests.length);
+      } catch (error) {
+        console.error("Error fetching friend requests:", error);
+      }
+    }
+    fetchFriendRequests();
+    },[]);
+
   // const queryClient = useQueryClient();
   // const {mutate:logoutMutation} = useMutation({
   //   mutationFn:logout,
@@ -31,8 +49,13 @@ const Navbar = () => {
           )}
           <div className="flex items-center gap-3 sm:gap-4">
             <Link to="/notifications">
-              <button className="btn btn-ghost btn-circle">
+              <button className="btn btn-ghost btn-circle relative">
                 <BellIcon className="h-6 w-6 text-base-content opacity-70"></BellIcon>
+                {friendRequestCount>0 && (
+                  <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full transform translate-x-1/2 -translate-y-1/2">
+                    {friendRequestCount}
+                  </span>
+                )}
               </button>
             </Link>
           </div>
